469779ef332167194951526d24c5f8b001bbfd1c
Rajendra565 <badatyarajendra186@gmail.com>
2023-11-07 14:50:56 +0530

    Add more logic to textuitils

--- textuitils-app/src/components/Forms.js
+++ textuitils-app/src/components/Forms.js
@@ -8,9 +8,7 @@ const handletoUpercase=()=>{
 const handletoLowercase=()=>{
   setText(text.toLowerCase())
 }
-const handletolength=()=>{
-  setlength(text.length-1)
-}
+
 
 const handletochange=(event)=>{
   setText(event.target.value)
@@ -21,24 +19,29 @@ const handletochange=(event)=>{
 
   return (
     <>
-      <h2>{heading}</h2>
+    <div className="container">
       <div className="mb-3">
+      <h2>{heading}</h2>
         <label htmlFor="exampleFormControlTextarea1" class="form-label">
-          Example textarea
+          Textarea
         </label>
         <textarea
           className="form-control"
           id="exampleFormControlTextarea1"
-          rows="3"
+          rows="8"
           value={text}
           onChange={handletochange}
         ></textarea>
       </div>
-      <p>finding the length:{length}</p>
+      <p>finding the length:{text.length}</p>
       <button className="btn btn-primary mx-3" onClick={handletoUpercase}>Click to uppercase</button>
       <button className="btn btn-primary mx-3" onClick={handletoLowercase}>Click to Lowercase</button>
-      <button className="btn btn-primary mx-3" onClick={handletolength}>Find length</button>
-
+     
+     </div>
+     <div className="container">
+      <p className="fs-1 ">Text summary</p>
+      <p className="fs-0.5">{text.split([" "]).length-1} word {text.length} characters</p>
+     </div>
     </>
   );
 }
