d378a36666eab93b907df2ff19f77a23acfb1c3b
Rajendra565 <badatyarajendra186@gmail.com>
2023-11-08 14:49:00 +0530

    changes the code

--- textuitils-app/src/components/Forms.js
+++ textuitils-app/src/components/Forms.js
@@ -13,7 +13,7 @@ const handletoLowercase=()=>{
 const handletochange=(event)=>{
   setText(event.target.value)
 }
-  const [text,setText]=useState("Enter ");
+  const [text,setText]=useState("");
   const [length,setlength]=useState(0);
   
 
@@ -31,17 +31,19 @@ const handletochange=(event)=>{
           rows="8"
           value={text}
           onChange={handletochange}
+          placeholder="Enter Text"
         ></textarea>
       </div>
       <p>finding the length:{text.length}</p>
       <button className="btn btn-primary mx-3" onClick={handletoUpercase}>Click to uppercase</button>
       <button className="btn btn-primary mx-3" onClick={handletoLowercase}>Click to Lowercase</button>
+      
      
      </div>
      <div className="container">
       <p className="fs-1 ">Text summary</p>
-      <p className="fs-0.5">{text.split([" "]).length-1} word {text.length} characters</p>
-      <h1>perview</h1>
+      <p className="fs-0.5"><b>{text.split([" "]).length-1}</b> word <b> {text.length}</b> characters</p>
+      <h2>perview</h2>
       <p>{text}</p>
      </div>
     </>
